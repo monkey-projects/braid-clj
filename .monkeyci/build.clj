b65dad8d1c9fc4895d3ce19205e006e04eddc3f2
Wout Neirynck <wout@monkey-projects.be>
2023-12-13 16:28:28 +0100

    Added publishing to build script

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,16 +1,20 @@
 (ns monkey.braid.build
   (:require [monkey.ci.build
+             [api :as api]
              [core :as c]
              [shell :as s]]))
 
-(c/defpipeline unit-test
-  [(s/bash "clj -X:junit:test")])
+(def unit-test
+  (s/bash "clj -X:junit:test"))
 
-(c/defpipeline uberjar
-  [(s/bash "clj -X:uber")])
+(defn deploy [ctx]
+  (s/bash "clj -X:jar:deploy"
+          {:extra-env (-> ctx
+                          (api/build-params)
+                          (select-keys ["CLOJARS_USERNAME" "CLOJARS_PASSWORD"]))}))
 
-(c/defpipeline build-image
-  [(s/bash "podman build -t braid-bot .")])
+(c/defpipeline publish
+  [unit-test
+   deploy])
 
-[unit-test
- uberjar]
+[publish]
